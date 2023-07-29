b3b3eeae91ef597a44e89fd84ef464d812b6c410
muhammad Danish <54522496+muhammad-danish1998@users.noreply.github.com>
2023-07-29 12:06:13 -0700

    dashboard work

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -27,6 +27,27 @@ export default function login() {
       */}
       <div className="flex min-h-full h-screen flex-1">
         <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+          <div className=" ">
+            <Link className="flex  cursor-pointer mb-4 text-2xl" href={"/"}>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-7 h-7"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+                  />
+                </svg>
+              </span>{" "}
+              Back to Home
+            </Link>
+          </div>
           <div className="mx-auto w-full max-w-sm lg:w-96">
             <div>
               <img
@@ -37,15 +58,16 @@ export default function login() {
               <h2 className="mt-8 text-4xl font-bold leading-9 tracking-tight text-gray-900">
                 Sign in to your account
               </h2>
-             
             </div>
 
             <div className="mt-10">
               <div>
                 <form action="#" method="POST" className="space-y-6">
-         
                   <div>
-                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
                       Email address
                     </label>
                     <div className="mt-2">
@@ -53,7 +75,6 @@ export default function login() {
                         id="email"
                         name="email"
                         type="email"
-                       
                         required
                         className="block w-full rounded-md px-2 border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       />
@@ -61,7 +82,10 @@ export default function login() {
                   </div>
 
                   <div>
-                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
                       Password
                     </label>
                     <div className="mt-2">
@@ -69,48 +93,55 @@ export default function login() {
                         id="password"
                         name="password"
                         type="password"
-                   
                         required
                         className="block w-full rounded-md px-2 border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       />
                     </div>
                   </div>
 
-                 
-
                   <div>
-                    <button
+                    <Link
+                      href={"/dashbord"}
                       type="submit"
                       className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                       Sign in
-                    </button>
+                    </Link>
                   </div>
                 </form>
               </div>
 
               <div className="mt-10">
                 <div className="relative">
-                  <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                  <div
+                    className="absolute inset-0 flex items-center"
+                    aria-hidden="true"
+                  >
                     <div className="w-full border-t border-gray-200" />
                   </div>
                   <div className="relative flex justify-center text-sm font-medium leading-6">
-                    <span className="bg-white px-6 text-gray-900">Or continue with</span>
+                    <span className="bg-white px-6 text-gray-900">
+                      Or continue with
+                    </span>
                   </div>
                 </div>
 
                 <div className=" flex justify-center">
-                 
-
-                <img src="/images/landing/signin/google.png" className="w-26 cursor-pointer h-24" alt="" />
-                 
+                  <img
+                    src="/images/landing/signin/google.png"
+                    className="w-26 cursor-pointer h-24"
+                    alt=""
+                  />
                 </div>
                 <p className="mt-2 text-lg leading-6 text-gray-500">
-                For Registration? {' '}
-                <Link href="/signup" className="underline text-lg font-semibold text-indigo-600 hover:text-indigo-500">
-                  Sign up
-                </Link>
-              </p>
+                  For Registration?{" "}
+                  <Link
+                    href="/signup"
+                    className="underline text-lg font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Sign up
+                  </Link>
+                </p>
               </div>
             </div>
           </div>
@@ -124,5 +155,5 @@ export default function login() {
         </div>
       </div>
     </>
-  )
+  );
 }
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,35 +1,33 @@
 import Link from "next/link";
 
-/*
-  This page requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
+
 export default function page() {
   return (
     <>
-      {/*
-        This page requires updating your template:
-
-        ```
-        <html class="h-full bg-white">
-        <body class="h-full">
-        ```
-      */}
+    
       <div className="flex min-h-full h-screen flex-1">
         <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
           <div className="mx-auto w-full max-w-sm lg:w-96">
-            <div className="mb-2">
-            <Link className=" font-bold font-heading text-2xl cursor-pointer" href={"/"}>  Back To Home</Link>
+            <div className=" ">
+              <Link className="flex  cursor-pointer mb-4 text-2xl" href={"/"}>
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-7 h-7"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+                    />
+                  </svg>
+                </span>{" "}
+                Back to Home
+              </Link>
             </div>
             <div>
               <img
@@ -38,12 +36,15 @@ export default function page() {
                 alt="Your Company"
               />
               <h2 className="mt-8 text-4xl font-bold leading-9 tracking-tight text-gray-900">
-                Sign up 
+                Sign up
               </h2>
               <p className="mt-2 text-sm leading-6 text-gray-500">
-                Not a member?{' '}
-                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                  Start  foreever free plan
+                Not a member?{" "}
+                <a
+                  href="#"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  Start foreever free plan
                 </a>
               </p>
             </div>
@@ -51,9 +52,12 @@ export default function page() {
             <div className="mt-10">
               <div>
                 <form action="#" method="POST" className="space-y-6">
-                <div>
-                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                     Full Name
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Full Name
                     </label>
                     <div className="mt-2">
                       <input
@@ -66,7 +70,10 @@ export default function page() {
                     </div>
                   </div>
                   <div>
-                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
                       Email address
                     </label>
                     <div className="mt-2">
@@ -74,7 +81,6 @@ export default function page() {
                         id="email"
                         name="email"
                         type="email"
-                       
                         required
                         className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       />
@@ -82,7 +88,10 @@ export default function page() {
                   </div>
 
                   <div>
-                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
                       Password
                     </label>
                     <div className="mt-2">
@@ -90,48 +99,61 @@ export default function page() {
                         id="password"
                         name="password"
                         type="password"
-                   
                         required
                         className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       />
                     </div>
                   </div>
 
-                 
-
                   <div>
-                    <button
+                    <Link
+                      href={"/dashbord"}
                       type="submit"
-                      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      className="flex w-full justify-center
+                      rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold
+                      leading-6 text-white shadow-sm hover:bg-indigo-500
+                      focus-visible:outline focus-visible:outline-2
+                      focus-visible:outline-offset-2
+                      focus-visible:outline-indigo-600"
                     >
+                      {" "}
                       Sign up
-                    </button>
+                    </Link>
                   </div>
                 </form>
               </div>
 
               <div className="mt-10">
                 <div className="relative">
-                  <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                  <div
+                    className="absolute inset-0 flex items-center"
+                    aria-hidden="true"
+                  >
                     <div className="w-full border-t border-gray-200" />
                   </div>
                   <div className="relative flex justify-center text-sm font-medium leading-6">
-                    <span className="bg-white px-6 text-gray-900">Or continue with</span>
+                    <span className="bg-white px-6 text-gray-900">
+                      Or continue with
+                    </span>
                   </div>
                 </div>
 
                 <div className=" flex justify-center">
-                 
-
-                <img src="/images/landing/signin/google.png" className="w-26 cursor-pointer h-24" alt="" />
-                 
+                  <img
+                    src="/images/landing/signin/google.png"
+                    className="w-26 cursor-pointer h-24"
+                    alt=""
+                  />
                 </div>
                 <p className="mt-2 text-lg leading-6 text-gray-500">
-                Already have an account?{' '}
-                <Link href="/login" className="underline text-lg font-semibold text-indigo-600 hover:text-indigo-500">
-                  Sign in
-                </Link>
-              </p>
+                  Already have an account?{" "}
+                  <Link
+                    href="/login"
+                    className="underline text-lg font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Sign in
+                  </Link>
+                </p>
               </div>
             </div>
           </div>
@@ -145,5 +167,5 @@ export default function page() {
         </div>
       </div>
     </>
-  )
+  );
 }
